f00931c6bd0079477ef8c563cdd192fa886be59e
rwdj <rwdavidsonjr@yahoo.com>
2018-03-20 20:55:37 -0400

    Autoload lib folder

--- csc312/config/application.rb
+++ csc312/config/application.rb
@@ -1,13 +1,21 @@
+# application.rb
+# @Author: Ricky
+# @E-Mail: rwdavidsonjr@yahoo.com
+# @Date:   2018-03-20 20:54:37
+# @Last Modified time: 2018-03-20 20:55:19
 require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
+# application.rb
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Csc312
   class Application < Rails::Application
+    # Auto load lib folder
+    config.autoload_paths << "#{Rails.root}/lib"
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
