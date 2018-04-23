f65a3a93b299e8c52c557f4d6aac637b87a9dd4a
rwdj <rwdavidsonjr@yahoo.com>
2018-04-22 21:12:54 -0400

    First commit

---
+++ csc312/app/models/twitter_client.rb
@@ -0,0 +1,7 @@
+# twitter_client.rb
+# @Author: Unknown
+# @E-Mail: na@mail.com
+# @Date:   2018-04-22 21:11:25
+# @Last Modified time: 2018-04-22 21:12:13
+class TwitterClient < ApplicationRecord
+end
